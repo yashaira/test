c2beca5d3d75fde6aca721b269518c97525015f3
yashaira <32114543+yashaira@users.noreply.github.com>
2018-07-24 15:54:35 -0400

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,7 @@
+<script src="jquery-3.3.1.min.js"></script>
+
+$(" ").click ( function (){
+  
+});
+// note in regualar jaavscript with the j query library not added you do object.click= function ();
+// so you have to make a fucntion for it first
